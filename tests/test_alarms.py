f3796a0efb79132eb94c7cef43b7a6ace52768d4
ar-india <alokranjan2005@gmail.com>
2020-04-24 20:16:13 +0530

    testcase implemented for new object sets
    changes -
    - made entire testcase Thread safe.  Pytest provides a mechanism by which tests can be run in parallell using multiple thread workers. hence i decided to implement syncronization of common functions such as create_volume(),delete_volume() etc.
    Although as of today we will not leverage threading as it won't provide much of a performance difference. but in future if more tests are added and ,say,the tests take half an hour to complete , then we can use threading to acheive faster results.
    for running pytest with multiple thread worker we need to install a new module called "pytest-xdist"
    - testcase related to volume, snapshot, volcoll have few common functionality and they should be made thread safe .
    
    -impleneted testcase for new object sets -
    
    README.md            test_access_control_records.py       test_arrays.py       test_events.py
    config.ini           test_active_directory_membership.py  test_audit_log.py    test_master_key.py
    conftest.py          test_alarms.py                       test_chap_users.py    test_application_categories.py       test_controllers.py
              test_application_servers.py          test_disks.py
    
    -Have written the readme file
    
    Signed-off-by: ar-india <alokranjan2005@gmail.com>

---
+++ tests/test_alarms.py
@@ -0,0 +1,10 @@
+# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
+#
+# @author alok ranjan
+from tests.nimbleclientbase import SKIPTEST, log_to_file as log
+
+
+'''AlarmsTestCase is not implemented due to external dependency '''
+
+
+log("**** AlarmsTestCase is not implemented due to external dependency *****'")
