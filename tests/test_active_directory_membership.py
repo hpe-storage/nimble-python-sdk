3cc98cfb47d8fa97a42b98f7b77e4d93d3b48b82
ar-india <alokranjan2005@gmail.com>
2020-04-25 17:32:35 +0530

    Revert "testcase implemented for new object sets"
    This reverts commit f3796a0efb79132eb94c7cef43b7a6ace52768d4.

--- tests/test_active_directory_membership.py
+++
@@ -1,9 +0,0 @@
-# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
-#
-# @author alok ranjan
-from tests.nimbleclientbase import log_to_file as log
-
-'''AD_MembershipTestCase is not implemented due to external dependency '''
-
-log("**** AD_MembershipTestCase is not implemented due to "
-    "external dependency *****'")
