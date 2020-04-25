f6545cb9c9098c810a2818e02ce3d71ddd2c5409
ar-india <alokranjan2005@gmail.com>
2020-04-25 17:56:07 +0530

    part2 - adding more testcases for nimble python sdk
    Signed-off-by: ar-india <alokranjan2005@gmail.com>

---
+++ tests/test_controllers.py
@@ -0,0 +1,68 @@
+# (c) Copyright 2020 Hewlett Packard Enterprise Development LP
+
+# @author alok ranjan
+import pytest
+import tests.nimbleclientbase as nimosclientbase
+from tests.nimbleclientbase import SKIPTEST, log_to_file as log
+from nimbleclient.v1 import exceptions
+
+
+'''Controller TestCase tests the controller object functionality '''
+
+
+@pytest.fixture(scope='module')
+def before_running_all_testcase(request):
+    log("**** Starting Tests for Controller TestCase *****\n")
+
+    def after_running_all_testcase():
+        log("**** Completed Tests for Controller TestCase *****\n")
+    request.addfinalizer(after_running_all_testcase)
+
+
+@pytest.fixture(scope='function')
+def setup_teardown_for_each_test(before_running_all_testcase, request):
+    # setup operations before yield is called
+    nimosclientbase.log_header(request.function.__name__)
+    yield setup_teardown_for_each_test
+    # teardown operations below
+    nimosclientbase.log_footer(request.function.__name__)
+
+
+@pytest.mark.skipif(SKIPTEST is True,
+                    reason="skipped this test as SKIPTEST variable is true")
+def test_controllers(setup_teardown_for_each_test):
+    resp = nimosclientbase.get_nimos_client().controllers.get()
+    assert resp is not None
+
+
+@pytest.mark.skipif(SKIPTEST is True,
+                    reason="skipped this test as SKIPTEST variable is true")
+def test_controllers_endrow_beyond(setup_teardown_for_each_test):
+    try:
+        resp = nimosclientbase.get_nimos_client().controllers.list(
+            detail=True, endRow=30)
+        assert resp is not None
+    except exceptions.NimOSAPIError as ex:
+        if "SM_end_row_beyond_total_rows" in str(ex):
+            log("Failed as expected.no rows present")
+        else:
+            raise ex
+
+
+@pytest.mark.skipif(SKIPTEST is True,
+                    reason="skipped this test as SKIPTEST variable is true")
+def test_select_fields_for_controllers(setup_teardown_for_each_test):
+    try:
+        resp = nimosclientbase.get_nimos_client().controllers.get(
+            fields="name,hostname")
+        assert resp is not None
+        assert resp.attrs.get("name") is not None
+        assert resp.attrs.get("hostname") is not None
+        # try asserting a value which was not querying
+        assert resp.attrs.get("port") is None
+        assert resp.attrs.get("creation_time") is None
+    except exceptions.NimOSAPIError as ex:
+        if "SM_end_row_beyond_total_rows" in str(ex):
+            log("Failed as expected")
+        else:
+            raise ex
