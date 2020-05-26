45d5bb3859f17c0f910c6f8fe1f3c5e76d290ad1
Michael Mattsson <michael.mattsson@gmail.com>
2020-05-26 11:19:47 -0700

    prepping setup.py for dist (#23)
    Signed-off-by: Michael Mattsson <michael.mattsson@nimblestorage.com>
    
    Co-authored-by: Michael Mattsson <michael.mattsson@nimblestorage.com>

--- setup.py
+++ setup.py
@@ -6,29 +6,30 @@ import setuptools
 
 __version__ = '1.0.0'
 
-with open("README.rst", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as requirements:
+with open('requirements.txt') as requirements:
     install_requires = requirements.readlines()
 
 setuptools.setup(
-    name="nimble-sdk",
+    name='nimble-sdk',
     version=__version__,
-    author="Suneethkumar Byadarahalli",
-    author_email="nimble-dcs-storage-automation-eng@hpe.com",
-    maintainer="Suneethkumar Byadarahalli, Alok Ranjan, George Costea",
-    keywords=["hpe", "nimble", "python", "sdk", "rest", "storage"],
-    requires=['requests'],
-    description="The Nimble Python SDK (client library) is a utility that can be leveraged to manage HPE Nimble Storage arrays.",
+    author='HPE Nimble Storage DCS',
+    author_email='nimble-dcs-storage-automation-eng@hpe.com',
+    maintainer='Suneethkumar Byadarahalli, Alok Ranjan, George Costea',
+    keywords='HPE Nimble Storage REST API SDK',
+    description='The HPE Nimble Storage SDK for Python (client library) is a utility that can be leveraged to manage HPE Nimble Storage arrays.',
     long_description=long_description,
-    long_description_content_type="text/x-rst",
-    license="Apache License, Version 2.0",
-    url="https://github.com/hpe-storage/nimble-python-sdk",
+    long_description_content_type='text/markdown',
+    license='Apache License, Version 2.0',
+    url='https://github.com/hpe-storage/nimble-python-sdk',
     project_urls={
-        "Documentation": "https://scod.hpedev.io/storage_automation/nimble_python_sdk/index.html",
-        "Code": "https://github.com/hpe-storage/nimble-python-sdk",
-        "Issue tracker": "https://github.com/hpe-storage/nimble-python-sdk/issues",
+        'Documentation': 'https://hpe-storage.github.io/nimble-python-sdk',
+        'Source': 'https://github.com/hpe-storage/nimble-python-sdk',
+        'Tracker': 'https://github.com/hpe-storage/nimble-python-sdk/issues',
+        'Community': 'https://hpedev.io'
+
     },
     packages=setuptools.find_packages(),
     install_requires=install_requires,
@@ -41,9 +42,7 @@ setuptools.setup(
         'Intended Audience :: System Administrators',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: MacOS',
+        'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries',
