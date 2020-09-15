609aa7a7712a386b8cac8ab5d7f6e862131601ff
prachi <prachi.ruparelia@hpe.com>
2020-09-15 06:06:07 -0700

    Fixes for few test failures
    Signed-off-by: Prachi <prachi.ruparelia@hpe.com>

--- tests/test_volume.py
+++ tests/test_volume.py
@@ -523,7 +523,7 @@ def test_delete_clone_volume(setup_teardown_for_each_test):
             raise ex
 
 
-@pytest.mark.skipif(SKIPTEST is True,
+@pytest.mark.skipif(SKIPTEST is True or nimosclientbase.get_nimos_client().pools.list().__len__() < 2,
                     reason="skipped this test as SKIPTEST variable is true")
 def test_bulk_move_volume(setup_teardown_for_each_test):
     try:
@@ -606,6 +606,8 @@ def test_bulk_set_dedupe(setup_teardown_for_each_test):
         if "SM_pool_dedupe_incapable" in str(ex):
             log("Failed as expected. pool is not"
                 "capable of hosting dedup volumes")
+        elif "Object is already in requested state" in str(ex):
+            log("Failed as expected. Object already in that state.")
         else:
             log(f"Failed with exception message : {str(ex)}")
             raise ex
